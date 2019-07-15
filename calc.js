0a0e567c31445816b4e11727f3571bec14b09757
Jordan-De-Sousa <desousa.jordan94@gmail.com>
2019-07-15 14:53:36 +0200

    make the 4 functions exportable

--- calc.js
+++ calc.js
@@ -1,22 +1,19 @@
-
-function add(x,y) {
+exports.add = function(x,y) {
     return x + y;
-}
+};
 
-
-function substract(x,y) {
+exports.substract = function(x,y) {
     return x - y;
-}
+};
 
+exports.multiply = function(x,y) {
+    return x * y;
+};
 
-function multiply(x,y) {
-    return x*y;
-}
+exports.divide = function(x,y) {
+    return x / y;
+};
 
-
-function divide(x,y) {
-    return x/y;
-}
 
 console.log(add(2,1))
 console.log(substract(2,1))
